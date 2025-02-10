a61fe4bb312461be2baa3efc86e4f22088ca15fc
Alejandro Rodríguez García <AlejandroRG1999@hotmail.com>
2025-02-10 12:10:12 +0100

    First deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { SharedModule } from './shared/shared.module';
+import { CountriesModule } from './countries/countries.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/pages/about-page/about-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  standalone: false,
+  templateUrl: './about-page.component.html',
+  styles: [
+
+  ],
+})
+export class AboutPageComponent {
+
+}
